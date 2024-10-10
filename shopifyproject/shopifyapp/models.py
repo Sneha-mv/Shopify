5d0123d403074c04cd0d0aacc05be169c47d2c48
Sneha-mv <snehamv424@gmail.com>
2024-10-10 11:36:02 +0300

    product detail page done

--- shopifyproject/shopifyapp/models.py
+++ shopifyproject/shopifyapp/models.py
@@ -20,6 +20,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
+    description = models.TextField(blank=True)
     price=models.DecimalField(max_digits=10,decimal_places=2)
     category=models.ForeignKey(Category,on_delete=models.CASCADE)
     image = models.ImageField(upload_to='product', blank=True)
@@ -39,3 +40,4 @@ class Product(models.Model):
     def __str__(self):
         return '{}'.format(self.name)
     
+
