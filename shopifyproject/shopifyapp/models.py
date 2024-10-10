9cd85b24555b258fa1d394a678920cc0cee963f7
Sneha-mv <snehamv424@gmail.com>
2024-10-10 20:50:09 +0300

    cart model created

--- shopifyproject/shopifyapp/models.py
+++ shopifyproject/shopifyapp/models.py
@@ -16,6 +16,7 @@ class Category(models.Model):
 
     def __str__(self):
         return '{}'.format(self.name)
+
     
 class Product(models.Model):
     name = models.CharField(max_length=250, unique=True)
@@ -39,5 +40,33 @@ class Product(models.Model):
 
     def __str__(self):
         return '{}'.format(self.name)
+    
+
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=250, blank=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['date_added']
+        db_table = 'Cart'
+
+    def __str__(self):
+        return self.cart_id
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'CartItem'
+
+    def sub_total(self):
+        return self.product.price * self.quantity
+
+    def __str__(self):
+        return self.product.name
     
 
--- shopifyproject/shopifyapp/views.py
+++ shopifyproject/shopifyapp/views.py
@@ -42,3 +42,4 @@ def SearchResult(request):
         products=Product.objects.all().filter(Q(name__contains=query) | Q(description__contains=query))
     return render(request,"search.html",{'query':query,'products':products})
 
+
