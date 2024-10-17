4145922f4c925079e66cd9f55f96f310eceda87e
Sneha-mv <snehamv424@gmail.com>
2024-10-17 17:58:58 +0300

    register page modified

--- shopifyproject/shopifyapp/views.py
+++ shopifyproject/shopifyapp/views.py
@@ -46,6 +46,15 @@ def SearchResult(request):
 
 
 def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        cpassword = request.POST.get('cpassword')
+
+        user = Shopify(username=username, email=email, password=password, cpassword=cpassword)
+        user.save()
+        return redirect('shopifyapp:index')
     return render(request,'register.html')
 
 
