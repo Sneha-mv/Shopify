f90c1dea09b6ccbd092de09454cb40e1706ff81d
Sneha-mv <snehamv424@gmail.com>
2024-10-25 12:42:40 +0300

    modified login and logout

--- shopifyproject/shopifyapp/admin.py
+++ shopifyproject/shopifyapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category,Product,Shopify,Cart,CartItem
+from .models import Category,Product,Shopify
 
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
@@ -17,14 +17,6 @@ admin.site.register(Product,ProductAdmin)
 admin.site.register(Shopify)
 
 
-class CartAdmin(admin.ModelAdmin):
-    list_display = ['cart_id', 'date_added']
-admin.site.register(Cart, CartAdmin)
 
-
-class CartItemAdmin(admin.ModelAdmin):
-    list_display = ['product', 'quantity', 'sub_total']
-    list_filter = ['cart', 'product']
-admin.site.register(CartItem, CartItemAdmin)
 
 
--- shopifyproject/shopifyproject/settings.py
+++ shopifyproject/shopifyproject/settings.py
@@ -66,7 +66,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'shopifyapp.context_processors.menu_links',
-                'shopifyapp.context_processors.counter',
                
             ],
         },
